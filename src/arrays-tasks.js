0c3fa77fad0a37c570eb5b12acd2ea1000a1647e
Sergey <sergey.grinkevich@gmail.com>
2024-02-13 00:52:40 +0200

    feat: add same task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -444,8 +444,20 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce((acc, cur, i) => {
+    if (cur % 2 !== 0) {
+      acc.push(i);
+    }
+    return acc;
+  }, []);
+  // const indices = [];
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] % 2 !== 0) {
+  //     indices.push(i);
+  //   }
+  // }
+  // return indices;
 }
 
 /**
@@ -458,8 +470,12 @@ function getIndicesOfOddNumbers(/* numbers */) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((num) => {
+    let hex = num.toString(16).toUpperCase();
+    hex = hex.padStart(6, '0');
+    return `#${hex}`;
+  });
 }
 
 /**
